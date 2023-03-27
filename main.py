bdbfff978ba8ef0bdeba319fe8a190a05b28e513
taffy21 <44449823+taffy21@users.noreply.github.com>
2023-03-27 22:25:12 +0200

    changed the main file extensively

--- main.py
+++ main.py
@@ -2,37 +2,56 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
-import string
+from kivy.metrics import dp
 from kivy.properties import StringProperty
-import hangman
+from kivy.uix.widget import Widget
+from kivy.uix.label import Label
+from random import choice
+import re
+
+class MyLabel(Label):
+    word = StringProperty
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.word = self.word_hider()
+        self.text = self.word
+
+    def begin_game(self):
+        self.word = choice(["CHUCK", "AEROPLANE", "DEVELOPMENT"])
+        return self.word
+
+    def word_hider(self):
+        count = len(self.begin_game())
+        return "_" * count
+
+    def combineListLetters(self, arg):   #not sure how this is meant to work @ ALL
+        word = ""
+        for i in arg:
+            word += i
+        return word
+
+    def wordPlayer(self):
+        playing = True
+        wrong_count = 0
+        complete = self.word_hider()
+        selected_word = self.begin_game()
+        while playing:
+            letter = self.ids.buttonA.text
+            if letter in selected_word:
+                indexes = [i.start() for i in re.finditer(letter, selected_word)]
+                for index in indexes:
+                    complete[index] = letter
+            else:
+                wrong_count += 1
+            if selected_word == self.combineListLetters(complete) or wrong_count == 10:
+                playing = False
 
 
 class MyBox(BoxLayout):
     pass
 
-
-
-class MyLetterGrid(GridLayout):
-    alphabet = list(string.ascii_uppercase)
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.cols = 5
-        self.buttons = []
-        for i in self.alphabet:
-            self.btn = Button(text=f'{i}')
-            self.btn.bind(on_press=self.pressed)
-            self.add_widget(self.btn)
-            self.buttons.append(self.btn)
-
-    def pressed(self, instance):
-        MyBox.ids.game_word.text = instance.text #this is where the code is dying
-
-
 class TrialGameApp(App):
-    def build(self):
-        return MyBox()
-
+    pass
 
 if __name__ == "__main__":
     TrialGameApp().run()
